4a946ffe71de014aa6ff2ec3e76971f74f3e8e33
Evgeniya <speranskaya1991@mail.ru>
2023-10-17 21:12:37 +0500

    не получается решить 66

--- task66/Program.cs
+++ task66/Program.cs
@@ -4,4 +4,29 @@
 // M = 4; N = 8. -> 30
 
 
+int SumNumbers (int numM, int numN)
+{
+    int sum = 0;
+    if (numM == numN) return numM;
+    else if (numM > numN) 
+    {
+        if (numM == numN) return 0;
+        sum = sum + SumNumbers(numM - 1);
+        return sum;
+    }
+    else
+    {
+        if (numM == numN) return 0;
+        sum = sum + SumNumbers(numN - 1);
+        return sum;
+    }
+
+}   
+Console.WriteLine("Введите натуральное число");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите натуральное число");
+int numberN = Convert.ToInt32(Console.ReadLine());
+int sumNumbers = SumNumbers(numberM, numberN);
+Console.Write($"M = {numberM}; N = {numberN} -> {sumNumbers}");
+
 
