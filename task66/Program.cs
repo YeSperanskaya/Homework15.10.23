99b52833f4da580136af1706608efcea13e1f7bb
Evgeniya <speranskaya1991@mail.ru>
2023-10-17 21:34:35 +0500

    все еще не получается решить 66

--- task66/Program.cs
+++ task66/Program.cs
@@ -4,24 +4,50 @@
 // M = 4; N = 8. -> 30
 
 
+
+
+int result = SumNumbers()
+
+
 int SumNumbers (int numM, int numN)
 {
     int sum = 0;
-    if (numM == numN) return numM;
-    else if (numM > numN) 
-    {
-        if (numM == numN) return 0;
-        sum = sum + SumNumbers(numM - 1);
-        return sum;
-    }
-    else
-    {
-        if (numM == numN) return 0;
-        sum = sum + SumNumbers(numN - 1);
-        return sum;
-    }
-
+    if(numM == numN) return numM+sum;
+    sum = sum + SumNumbers(numM + 1, numN);
+    return sum;
 }   
+
+
+
+// int ExaminationMinMax (int numM, int numN)
+// {
+//     int sum = 0;
+//     sum = sum + 
+//     if 
+//     if (numM == numN) return numM;
+//     else if(numM > numN)
+//     {
+
+//     }
+// }
+// int SumNumbers (int numM, int numN)
+// {
+//     int sum = 0;
+//     if (numM == numN) return numM;
+//     else if (numM > numN) 
+//     {
+//         if (numM == numN) return 0;
+//         sum = sum + SumNumbers(numM - 1);
+//         return sum;
+//     }
+//     else
+//     {
+//         if (numM == numN) return 0;
+//         sum = sum + SumNumbers(numN - 1);
+//         return sum;
+//     }
+
+// }   
 Console.WriteLine("Введите натуральное число");
 int numberM = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите натуральное число");
